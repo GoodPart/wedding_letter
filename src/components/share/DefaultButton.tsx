8da93b09118e0f85ff29669085eff33f00e10896
GoodPart <pks940122@gmail.com>
2024-09-18 14:24:38 +0900

    og 태그 텍스트 수정 및 공유 버튼 추가

--- src/components/share/DefaultButton.tsx
+++ src/components/share/DefaultButton.tsx
@@ -2,8 +2,14 @@ import React from "react";
 import ButtonStyle from "./style";
 
 export const DefaultButton = () => {
+  function copyToClipBoard() {
+    window.navigator.clipboard.writeText(
+      "https://fascinating-sprite-94efd8.netlify.app/"
+    );
+    alert("모바일청첩장 주소를 복사되었습니다");
+  }
   return (
-    <ButtonStyle bg="gray" color="#fff">
+    <ButtonStyle bg="gray" color="#fff"  onClick={()=>copyToClipBoard()}>
       공유하기
     </ButtonStyle>
   );
